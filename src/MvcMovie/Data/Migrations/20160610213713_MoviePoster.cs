1609d63623e0a06ef64be794354e34fb1cf622c6
Temp1 <Temp1@TEMP1-PC>
2016-06-13 10:10:48 -0700

    Add project files.

---
+++ src/MvcMovie/Data/Migrations/20160610213713_MoviePoster.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Data.Migrations
+{
+    public partial class MoviePoster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MoviePoster",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MoviePoster",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                nullable: true);
+        }
+    }
+}
---
+++ src/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                     new Movie
+                     {
+                         Title = "When Harry Met Sally",
+                         ReleaseDate = DateTime.Parse("1989-1-11"),
+                         Genre = "Romantic Comedy",
+                         Rating = "R",
+                         Trailer= "https://www.youtube.com/watch?v=V8DgDmUHVto",
+                         MoviePoster= "http://ia.media-imdb.com/images/M/MV5BMjE0ODEwNjM2NF5BMl5BanBnXkFtZTcwMjU2Mzg3NA@@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+                         Price = 7.99M
+                     },
+
+                     new Movie
+                     {
+                         Title = "Ghostbusters ",
+                         ReleaseDate = DateTime.Parse("1984-3-13"),
+                         Genre = "Comedy",
+                         Rating = "PG",
+                         Trailer = "https://www.youtube.com/watch?v=w3ugHP-yZXw",
+                         MoviePoster = "http://cafmp.com/wp-content/uploads/2015/11/Ghostbusters.jpg",
+                         Price = 8.99M
+                     },
+
+                     new Movie
+                     {
+                         Title = "Ghostbusters 2",
+                         ReleaseDate = DateTime.Parse("1986-2-23"),
+                         Genre = "Comedy",
+                         Rating = "PG",
+                         Trailer = "https://www.youtube.com/watch?v=UnzH75FlwvU",
+                         MoviePoster = "https://upload.wikimedia.org/wikipedia/en/0/01/Ghostbusters_ii_poster.jpg",
+                         Price = 9.99M
+                     },
+
+                   new Movie
+                   {
+                       Title = "Rio Bravo",
+                       ReleaseDate = DateTime.Parse("1959-4-15"),
+                       Genre = "Western",
+                       Rating = "R",
+                       Trailer = "https://www.youtube.com/watch?v=WPO12ZzGS84",
+                       MoviePoster = "http://www.doctormacro.com/Images/Posters/R/Poster%20-%20Rio%20Bravo_01.jpg",
+                       Price = 3.99M
+                   }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
